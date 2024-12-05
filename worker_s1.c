95d53d04cc108d35e3c97e6bd300a52858b62173
Maciej Bober <maciekbober03@gmail.com>
2024-12-05 10:38:15 +0100

    Added worker1 functionality.

--- worker_s1.c
+++ worker_s1.c
@@ -2,7 +2,7 @@
  * Operating Systems  (2INCO)  Practical Assignment
  * Interprocess Communication
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
+ * Maciej Bober 1809628
  * STUDENT_NAME_2 (STUDENT_NR_2)
  *
  * Grading:
